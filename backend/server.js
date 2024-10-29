30bf41aec8d72f67d2143c7cb73527a72e081936
Paul Rose <ptrose78@gmail.com>
2024-10-29 10:25:26 -0500

    Removed code from server.js since can't Node.js will not connect to database in cPanel; Updated cakeSlice with CreateAsyncThunk to fetch from customized WordPress API endpoint

--- backend/server.js
+++ backend/server.js
@@ -9,34 +9,6 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log('backend')
-
-// MySQL connection
-const db = mysql.createConnection({
-  host     : process.env.HOST,
-  user     : process.env.USER,
-  password : process.env.PASSWORD,
-  database : process.env.DATABASE
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error('Database connection failed:', err.stack);
-        return;
-    }
-    console.log('Connected to database.');
-});
-
-// API endpoint to get cake data
-app.get('/api/cakes', (req, res) => {
-    db.query('SELECT * FROM cakes', (error, results) => {
-        if (error) {
-            return res.status(500).json({ error });
-        }
-        res.json(results);
-    });
-});
-
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
