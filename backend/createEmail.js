ff244510182c060f943187ec88ae45432376139f
Paul Rose <ptrose78@gmail.com>
2024-11-16 12:49:39 -0600

    Created an email notification to the owner following a customer order and payment.

---
+++ backend/createEmail.js
@@ -0,0 +1,28 @@
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+const apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.YOUR_API_V3_KEY;
+
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+async function sendTransactionalEmail(subject, senderName, senderEmail, htmlContent, recipientEmail) {
+    const email = {
+        sender: { name: senderName, email: senderEmail },
+        to: [{ email: recipientEmail }], // Correct format
+        subject: subject,
+        htmlContent: htmlContent,
+    };
+
+    try {
+        const response = await apiInstance.sendTransacEmail(email);
+        console.log("Email sent successfully:", response);
+        return response;
+    } catch (error) {
+        console.error("Error sending email:", error);
+        throw error;
+    }
+}
+
+module.exports = { sendTransactionalEmail };
