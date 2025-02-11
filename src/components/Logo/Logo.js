0d3cadcd2c2d06ae59700ade44b112f35f751f18
Paul Rose <ptrose78@gmail.com>
2025-02-10 21:14:33 -0600

    Adjusted styling.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,32 +1,40 @@
 // src/pages/Logo.js
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchImageFromStorage, selectImages } from '../../features/logo/logoSlice';
 import './Logo.css';  // Import the external CSS file
 
 const Logo = () => {
-    const dispatch = useDispatch();
-    const {images} = useSelector(selectImages);
+  const dispatch = useDispatch();
+  const { images } = useSelector(selectImages);
+  const [loading, setLoading] = useState(true); // Track loading state
 
-useEffect(() => {
-    console.log('use Effect Logo')
-    const getData = async() => {
-        if (!images) {
-            await dispatch((fetchImageFromStorage()))
-        }
-    }
+  useEffect(() => {
+    const getData = async () => {
+      if (!images) {
+        await dispatch(fetchImageFromStorage());
+      } else {
+        setLoading(false); // Set loading to false once images are fetched
+      }
+    };
     getData();
-}, [dispatch, images])
+  }, [dispatch, images]);
 
-const placeholderImage = "https://source.unsplash.com/random/150x150";
- // Replace with your placeholder path
+  const placeholderImage = "https://source.unsplash.com/random/150x150"; // Placeholder image
 
-return (
-    <img 
-        className="logo" 
-        src={images ? images[0].image: placeholderImage} 
-        alt="Logo" 
-    />)
+  return (
+    <div className="logo-container">
+      {loading ? (
+        <div className="logo-loading">Loading...</div> // Show loading message while images are being fetched
+      ) : (
+        <img
+          className="logo"
+          src={images ? images[0].image : placeholderImage}
+          alt="Logo"
+        />
+      )}
+    </div>
+  );
 };
 
 export default Logo;
