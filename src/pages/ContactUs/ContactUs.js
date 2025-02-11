763e4d51ad1df2ae8ef708d4a0e554cadae57446
Paul Rose <ptrose78@gmail.com>
2025-02-10 21:29:07 -0600

    Updated styling.

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ContactForm from "../../components/ContactForm/ContactForm";
+import "./ContactUs.css";
 
 const ContactUs = () => {
 
     return (
-        <div class="container">
+        <div className="contact-us-container">
             <h1>Contact Us</h1>
             <ContactForm />
         </div>
